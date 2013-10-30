c81e7e0336b4545986b4f946f4894a90d258ab46
TakesxiSximada <takesxi.sximada@gmail.com>
2013-10-30 15:55:50 +0900

    create setup scripts.

---
+++ setup.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+README_PATH = os.path.join(ROOT, 'README')
+
+try:
+    with open(README_PATH, 'rb') as fp:
+        long_desc = fp.read()
+except:
+    long_desc = ''
+    
+requires = ['selenium']
+
+setup(
+    name='pywad',
+    version='0.0.1',
+    url='https://bitbucket.org/takesxi_sximada/pywad',
+    download_url='https://bitbucket.org/takesxi_sximada/pywad',
+    license='BSD'
+    author='TakesxiSximada'
+    author_email='takesxi.sximada@gmail.com'
+    description='The Python Web Auto Drive framework using selenium.webdriver.',
+    long_description=long_desc,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment :: Mozilla',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Natural Language :: Esperanto',
+        'Natural Language :: Japanese',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Browsers',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+        ],
+    platforms='any',
+    include_package_data=True,
+    install_requires=requires,
+    packages=find_packages(),
+    )
